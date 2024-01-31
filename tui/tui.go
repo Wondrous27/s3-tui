03223d025c9ecd5f97e0365c20ad8e3ec8fb964c
Berk Güler <berk.guler@windowslive.com>
2024-01-31 15:27:34 +0300

    remove debug.log

--- tui/tui.go
+++ tui/tui.go
@@ -2,7 +2,6 @@ package tui
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/Wondrous27/s3-tui/bucket"
@@ -12,21 +11,15 @@ import (
 )
 
 func StartTea(br bucket.S3Repository, or object.S3Repository) {
-	if f, err := tea.LogToFile("debug.log", "help"); err != nil {
-		fmt.Println("Couldn't open a file for logging:", err)
-		os.Exit(1)
-	} else {
-		defer func() {
-			err = f.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
-	}
 	constants.Br = &br
 	constants.Or = &or
 
-	m, _ := InitBuckets() // TODO: can we acknowledge this error
+	m, err := InitBuckets()
+	if err != nil {
+		fmt.Println("Error initializing buckets", err)
+		os.Exit(1)
+	}
+
 	constants.P = tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := constants.P.Run(); err != nil {
 		fmt.Println("Error running program:", err)
